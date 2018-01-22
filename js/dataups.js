57f719e22d3f52a2bd6a5d0758f2051e98dc10c3
YoannaHS <yoannahernandez.s@gmail.com>
2018-01-22 18:15:22 -0500

    Añadiendo data

--- js/dataups.js
+++ js/dataups.js
@@ -1,6 +1,10 @@
 data = {
     Gladiator: {
-        url: ,
-        info: 
+        url: 'assets/videos/Gladiator.mp4',
+        info: 'Estrenada el 5 de mayo de 2000, Gladiator fue un éxito tanto por la crítica como del público y contribuyó al breve resurgir del cine épico histórico. La película recibió numerosos premios y nominaciones, incluidos cinco Óscar en la 73º edición de la Academia estadounidense entre los que se destaca el de mejor película y mejor actor para Crowe.'
+    },
+    Braveheart: {
+        url: 'assets/videos/Braveheart.mp4',
+        info: 'Braveheart es una película estadounidense histórica-dramática de 1995 dirigida, producida y protagonizada por Mel Gibson. La cinta épica, basada en la vida de William Wallace, un héroe nacional escocés que participó en la Primera Guerra de Independencia de Escocia, fue ganadora de cinco Premios de la Academia, incluyendo el Óscar a la Mejor película. La batalla final de la película fue más real de lo que se piensa, ya que se eligieron como extras a los hombres de dos pueblos rivales. Muchos de ellos tuvieron que recibir asistencia médica después de la batalla.'
     }
 }
